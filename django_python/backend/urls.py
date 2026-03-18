d160d389623ea25c7c0c4ff0c6f7fc5325ed1369
dwh <1724865912@qq.com>
2026-03-18 17:37:33 +0800

    初始化项目提交

--- django_python/backend/urls.py
+++ django_python/backend/urls.py
@@ -5,28 +5,28 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, re_path
+    2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import re_path, include
 from django.conf import settings
 from django.contrib import admin
 from django.views.static import serve
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     # Frontend pages rendered by Django templates
-    url(r'^', include('web.urls')),
+    re_path(r'^', include('web.urls')),
 ]
 
 # Serve old project's /public/* assets in development (DEBUG=True)
 if settings.DEBUG:
     urlpatterns = urlpatterns + [
-        url(r'^public/(?P<path>.*)$', serve, {'document_root': settings.PUBLIC_DIR}),
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^public/(?P<path>.*)$', serve, {'document_root': settings.PUBLIC_DIR}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
